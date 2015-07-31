205c268c54d6b5ab34d46cf9153de09f0b1f2f52
Mikael Andersson <mikael@invoke.se>
2015-07-31 15:40:54 +0200

    additional properties for game model

--- models/game.rb
+++ models/game.rb
@@ -1,5 +1,13 @@
 class Game
   include DataMapper::Resource
-  property :id,   Serial
-  property :name, String
+
+  property :id,             Serial
+  property :title,          String
+  property :bgg_id,         Integer
+  property :min_players,    Integer
+  property :max_players,    Integer
+  property :duration,       Integer
+  property :year_published, Integer
+  property :expansion,      Boolean
+  property :image_url,      String
 end
